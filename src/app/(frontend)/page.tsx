32aada744279f26f5f5742293243a2d0c99fa02c
Teomago <teo.ibagon@gmail.com>
2025-07-31 06:36:21 -0500

    Last commit
    Experiment finished: Accordion in HeroUI is not working, in general HeroUI is a good option, but the installation and corrupted updates are not a good reason to continue

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -8,14 +8,17 @@ import {
   CardHeader,
   CardBody,
   CardFooter,
+  Code,
   Divider,
   Link,
   Button,
   Image as HeroUiImage,
 } from '@heroui/react'
+import { Accordion, AccordionItem } from '@heroui/react'
 
 import config from '@/payload.config'
 import './styles.css'
+import Hero from './hero'
 
 export default async function HomePage() {
   const headers = await getHeaders()
@@ -26,94 +29,101 @@ export default async function HomePage() {
   const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
 
   return (
-    <div className="flex flex-col h-screen h-max-auto max-w-full items-center justify-center overflow-hidden bg-black text-white">
-      <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
-        <Card className="col-span-12 sm:col-span-4 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col items-start!">
-            <p className="text-tiny text-white/60 uppercase font-bold">What to watch</p>
-            <h4 className="text-white font-medium text-large">Stream the Acme event</h4>
-          </CardHeader>
+    <div className="flex flex-col h-auto h-max-auto max-w-full items-center overflow-hidden bg-black text-white">
+      <div className="2xl:w-full 2xl:h-auto my-8 px-36 flex items-center justify-center">
+        <div className="flex flex-col 2xl:w-2/5 px-24">
+          <h1 className="text-6xl font-bold">Next.js Site Template For Agency and Portfolio</h1>
+          <p className="my-8 text-orange-400">
+            Elevate your online presence and impress clients effortlessly! — featuring essential
+            integrations, pre-built pages, and customizable components. Modern, responsive design
+            with fast loading and SEO optimization.
+          </p>
+          <Button
+            className="w-1/4 bg-linear-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+            radius="md"
+          >
+            Button
+          </Button>
+        </div>
+        <div className="2xl:w-3/5 px-6">
           <HeroUiImage
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
-            src="https://heroui.com/images/card-example-4.jpeg"
+            isBlurred
+            alt="HeroUI Album Cover"
+            className=""
+            src="https://images.wallpaperscraft.com/image/single/soldier_helmet_art_123765_1920x1080.jpg"
           />
-        </Card>
-        <Card className="col-span-12 sm:col-span-4 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col items-start!">
-            <p className="text-tiny text-white/60 uppercase font-bold">Plant a tree</p>
-            <h4 className="text-white font-medium text-large">Contribute to the planet</h4>
-          </CardHeader>
-          <HeroUiImage
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
-            src="https://heroui.com/images/card-example-3.jpeg"
-          />
-        </Card>
-        <Card className="col-span-12 sm:col-span-4 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col items-start!">
-            <p className="text-tiny text-white/60 uppercase font-bold">Supercharged</p>
-            <h4 className="text-white font-medium text-large">Creates beauty like a beast</h4>
-          </CardHeader>
-          <HeroUiImage
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
-            src="https://heroui.com/images/card-example-2.jpeg"
-          />
-        </Card>
-        <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
-          <CardHeader className="absolute z-10 top-1 flex-col items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">New</p>
-            <h4 className="text-black font-medium text-2xl">Acme camera</h4>
-          </CardHeader>
-          <HeroUiImage
-            removeWrapper
-            alt="Card example background"
-            className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-            src="https://heroui.com/images/card-example-6.jpeg"
-          />
-          <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
-            <div>
-              <p className="text-black text-tiny">Available soon.</p>
-              <p className="text-black text-tiny">Get notified.</p>
-            </div>
-            <Button className="text-tiny" color="primary" radius="full" size="sm">
-              Notify Me
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
-          <CardHeader className="absolute z-10 top-1 flex-col items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">Your day your way</p>
-            <h4 className="text-white/90 font-medium text-xl">Your checklist for better sleep</h4>
-          </CardHeader>
-          <HeroUiImage
-            removeWrapper
-            alt="Relaxing app background"
-            className="z-0 w-full h-full object-cover"
-            src="https://heroui.com/images/card-example-5.jpeg"
-          />
-          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-            <div className="flex grow gap-2 items-center">
-              <HeroUiImage
-                alt="Breathing app icon"
-                className="rounded-full w-10 h-11 bg-black"
-                src="https://heroui.com/images/breathing-app-icon.jpeg"
-              />
-              <div className="flex flex-col">
-                <p className="text-tiny text-white/60">Breathing App</p>
-                <p className="text-tiny text-white/60">Get a good night&#39;s sleep.</p>
-              </div>
-            </div>
-            <Button radius="full" size="sm">
-              Get App
-            </Button>
-          </CardFooter>
-        </Card>
+        </div>
       </div>
+
+      <div className="2xl:w-full 2xl:h-auto my-8 px-36 flex items-center justify-center">
+        <div className="2xl:w-2/5 px-6">
+          <HeroUiImage
+            isBlurred
+            alt="HeroUI Album Cover"
+            className=""
+            src="https://images.wallpaperscraft.com/image/single/code_text_colorful_140555_1080x1920.jpg"
+          />
+        </div>
+        <div className="flex flex-col 2xl:w-3/5 px-24">
+          <div className="w-full flex">
+            <div className="flex flex-col w-1/2 px-2">
+              <h1 className="text-6xl font-bold">Arte</h1>
+              <p className="my-8 text-orange-400">
+                Elevate your online presence and impress clients effortlessly! — featuring essential
+                integrations, pre-built pages, and customizable components. Modern, responsive
+                design with fast loading and SEO optimization.
+              </p>
+            </div>
+            <div className="flex flex-col w-1/2 px-2">
+              <h1 className="text-6xl font-bold">Programacion</h1>
+              <p className="my-8 text-orange-400">
+                Elevate your online presence and impress clients effortlessly! — featuring essential
+                integrations, pre-built pages, and customizable components. Modern, responsive
+                design with fast loading and SEO optimization.
+              </p>
+            </div>
+          </div>
+          <h1 className="text-6xl font-bold px-2">
+            Multiples disciplinas como base para solucionar lo que se te ocurre
+          </h1>
+        </div>
+      </div>
+
+      <div className="2xl:w-full 2xl:h-auto my-8 px-36 flex items-center justify-center">
+        <div className="flex flex-col 2xl:w-3/5 px-24">
+          <div className="w-full flex">
+            <div className="flex flex-col w-1/2 px-2">
+              <h1 className="text-6xl font-bold">Arte</h1>
+              <p className="my-8 text-orange-400">
+                Elevate your online presence and impress clients effortlessly! — featuring essential
+                integrations, pre-built pages, and customizable components. Modern, responsive
+                design with fast loading and SEO optimization.
+              </p>
+            </div>
+            <div className="flex flex-col w-1/2 px-2">
+              <h1 className="text-6xl font-bold">Programacion</h1>
+              <p className="my-8 text-orange-400">
+                Elevate your online presence and impress clients effortlessly! — featuring essential
+                integrations, pre-built pages, and customizable components. Modern, responsive
+                design with fast loading and SEO optimization.
+              </p>
+            </div>
+          </div>
+          <h1 className="text-6xl font-bold px-2">
+            Multiples disciplinas como base para solucionar lo que se te ocurre
+          </h1>
+        </div>
+        <div className="2xl:w-2/5 px-6">
+          <HeroUiImage
+            isBlurred
+            alt="HeroUI Album Cover"
+            className=""
+            src="https://images.wallpaperscraft.com/image/single/code_text_colorful_140555_1080x1920.jpg"
+          />
+        </div>
+      </div>
+
+      <div className="2xl:w-full 2xl:h-auto my-8 px-36 flex items-center justify-center"></div>
     </div>
   )
 }
